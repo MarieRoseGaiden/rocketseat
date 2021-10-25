93b922f8ddecb97e46c928bc773c4556a3c86a09
MarieRoseGaiden <marierosegaiden@gmail.com>
2021-10-25 12:28:43 -0300

    first commit

---
+++ nlw2/src/database/fake_data.js
@@ -0,0 +1,24 @@
+const proffys = [
+    {
+        name: "Diego Fernandes",
+        avatar: "https://avatars.githubusercontent.com/u/2254731?v=4",
+        whatsapp: "38997338401",
+        bio: "Entusiasta das melhores tecnologias de química avançada<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões",
+        subject: "Química",
+        cost: "20",
+        weekday: "[0]",
+        time_from: "[720]",
+        time_to: "[1220]"
+    },
+    {
+        name: "Daniele Evangelista",
+        avatar: "https://avatars.githubusercontent.com/u/4605945?v=4",
+        whatsapp: "38997338401",
+        bio: "Entusiasta das melhores tecnologias de química avançada<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões",
+        subject: "Química",
+        cost: "20",
+        weekday: "[1]",
+        time_from: "[720]",
+        time_to: "[1220]"
+    }
+]
